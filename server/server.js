cf5607b60a2edc400c47b129152a3391df45db1e
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2022-06-24 08:31:09 +0200

    Test add user endpoint

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const parser = require('body-parser')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
+const User = require('./models/user')
+
 dotenv.config()
 const app = express()
 
@@ -12,7 +14,7 @@ app.use(morgan('dev'))
 app.use(parser.json())
 app.use(parser.urlencoded({ extended: false }))
 
-mongoose.connect(process.env.DB_URI, { useUnifiedTopology: false }, err => {
+mongoose.connect(process.env.LOCAL_DB_URI, { useUnifiedTopology: false }, err => {
   if (err) console.log(err)
   else console.log('MongoDB is connected..')
 })
@@ -23,7 +25,19 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-  console.log(req.body)
+  const user = new User()
+
+  user.name = req.body.name
+  user.email = req.body.email
+  user.password = req.body.password
+
+  user.save(err => {
+    if (err) {
+      res.json(err)
+    } else {
+      res.json("sucess")
+    }
+  })
 })
 
 app.listen(3000, err => {
