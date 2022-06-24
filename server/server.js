ce9071601d0a5a44ab1880a9aa6bb6c23bf3d432
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2022-06-24 08:24:40 +0200

    Connect to database

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const morgan = require('morgan')
 const parser = require('body-parser')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
+dotenv.config()
 const app = express()
 
 // Middlewares
 app.use(morgan('dev'))
 app.use(parser.json())
 app.use(parser.urlencoded({ extended: false }))
+
+mongoose.connect(process.env.DB_URI, { useUnifiedTopology: false }, err => {
+  if (err) console.log(err)
+  else console.log('MongoDB is connected..')
+})
 
 
 app.get('/', (req, res) => {
@@ -22,6 +30,6 @@ app.listen(3000, err => {
   if (err) {
     console.log('err :>> ', err)
   } else {
-    console.log('Server is listening at port 3000 ...');
+    console.log('Server is listening at port 3000 ...')
   }
 })
