a67d5e7308f130838947608bb4348e8cdc4ffa4e
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2022-07-28 14:36:14 +0200

    Delete & update address

--- server/routes/address.js
+++ server/routes/address.js
@@ -42,5 +42,55 @@ router.get('/addresses/:user', verifyToken, async (req, res) => {
   }
 })
 
+// Update a particular address endpoint
+router.put('/addresses/:id', verifyToken, async (req, res) => {
+  try {
+    const address = await Address.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $set: {
+          user: req.decoded._id,
+          country: req.body.country,
+          fullName: req.body.fullName,
+          streetAddress: req.body.streetAddress,
+          city: req.body.city,
+          state: req.body.state,
+          zipCode: req.body.zipCode,
+          phoneNumber: req.body.phoneNumber,
+          deliverInstructions: req.body.deliverInstructions,
+          securityCode: req.body.securityCode
+        }
+      },
+      {
+        upsert: true, //  inserts a new document if no document matches 
+        new: true //  return the new updated document
+      }
+    )
+
+    res.json({ success: true, updated: address })
+
+  } catch (error) {
+    res.status(500).json({ success: false, error })
+  }
+})
+
+// Delete a particular address endpoint
+router.delete('/addresses/:id', verifyToken, async (req, res) => {
+  try {
+    const deleted = await Address.findOneAndDelete(
+      {
+        _id: req.params.id,
+        user: req.decoded._id,
+      }
+    )
+
+    if (deleted) {
+      res.json({ success: true, message: 'Deleted' })
+    }
+
+  } catch (error) {
+    res.status(500).json({ success: false, error })
+  }
+})
 
 module.exports = router
